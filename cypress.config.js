48ba22c01bc8daad4f604c7a501064151b20bcdc
Nelly Mejias <nelly.mejias@applydigital.com>
2024-06-26 17:52:36 -0400

    Actualizacion viewport

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,11 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  viewportWidth: 414,
+  viewportHeight: 896,
+  chromeWebSecurity: false,
   e2e: {
+    baseUrl: "https://www.adidas.cl/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
