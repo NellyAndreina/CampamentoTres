78a3300292e209aaecbed5887c0f3ea232eb4b11
Nelly Mejias <nelly.mejias@applydigital.com>
2024-07-09 10:03:06 -0400

    Actualización de cambios en login

--- cypress/e2e/selectCategory.cy.js
+++ cypress/e2e/selectCategory.cy.js
@@ -1,8 +1,10 @@
 import CommonPage from "../page/commonPage";
 import CategoryPage from "../page/categoryPage";
+import { LoginRegister } from "../Page/loginPage";
 
 const categoryPage = new CategoryPage();
-const commonPage = new CommonPage();
+//const commonPage = new CommonPage();
+const login = new LoginRegister();
 const userData = require('../fixtures/personalInfo.json');
 
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -12,11 +14,16 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 describe('Pruebas de la categoria de productos', () => {
     beforeEach(() => {
         cy.visit('https://www.casaideas.cl/');
-        cy.wait(5000) //timeout
+        cy.wait(10000) //timeout
         cy.get('.decline-button').click({force:true});
-        commonPage.userLogin(userData.email, userData.password);
+        login.clickIconLogin();
+        //commonPage.userLogin(userData.email, userData.password);
       });
     it('Mostrar las tarjetas en la categoria', () => {
+        login.typeEmail(userData.email);
+        login.typePassword(userData.password);  
+        login.clickOnLogin();
+        cy.wait(10000) //timeout
         categoryPage.visitMobileMenu();
         categoryPage.visitMenuProductos();
         categoryPage.visitMenuOutlet();
--- cypress/page/loginPage.js
+++ cypress/page/loginPage.js
@@ -3,15 +3,16 @@ export class LoginRegister {
     visitPageLogin () {
       cy.visit('/');
       //cy.get('.close').click();
+    }
+    clickIconLogin = ()  => {
       cy.get('.UserNav_list__dJ5pz > :nth-child(2)').click({force:true});
     }
-
     typeEmail = (email) => {
-      cy.get('#email').type(email,{force:true});
+      cy.get('#email').click().type(email,{force:true});
     }
 
     typePassword = (password) => {
-      cy.get('#password').type(password,{force:true});
+      cy.get('#password').click().type(password,{force:true});
     }
 
     clickOnLogin(){
